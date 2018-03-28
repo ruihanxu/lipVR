8c2dbb7e761c6cd37a498421d8df2882e4d9746a
Ruihan Xu <ruihanxu@Ruihans-MacBook-Pro.local>
2018-03-28 16:49:05 -0400

    add harris corner

--- mouth_detector.py
+++ mouth_detector.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 mouth_cascade = cv2.CascadeClassifier('haarcascade_mcs_mouth.xml')
-print(mouth_cascade.empty())
+
 if mouth_cascade.empty():
   raise IOError('Unable to load the mouth cascade classifier xml file')
 
@@ -14,13 +14,20 @@ while True:
     frame = cv2.resize(frame, None, fx=ds_factor, fy=ds_factor, interpolation=cv2.INTER_AREA)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
+
     mouth_rects = mouth_cascade.detectMultiScale(gray, 1.7, 11)
     for (x,y,w,h) in mouth_rects:
         y = int(y - 0.15*h)
-        cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 3)
+        cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 2)
+        
+        # find harris corner
+        gray_corner = np.float32(gray)
+        dst = cv2.cornerHarris(gray,2,3,0.04)
+        frame[dst>0.01*dst.max()] = [0,0,255]
+        
         break
 
-    cv2.imshow('Mouth Detector', frame)
+    cv2.imshow('Mouth Detector', frame[y:y+h,x:x+w])
 
     c = cv2.waitKey(1)
     if c == 27:
